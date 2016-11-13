37a82b6236c17cb1503142974a9140efe0e8cfda
Richard Henry <r.henry1989@gmail.com>
2016-11-13 16:05:44 +0000

    players can lose and gain possession of the ball

--- __tests__/match.test.js
+++ __tests__/match.test.js
@@ -9,4 +9,21 @@ describe( "Football Match", () => {
     })
   })
 
+  describe( "on LOSE_POSSESSION", () => {
+    test( "player loses possession", () => {
+      const newState = matchReducer( match, { type: "LOSE_POSSESSION" } );
+      expect( newState.getIn( ['home', 0, 'possession'] ) ).toBe( false );
+    })
+  })
+
+  describe( "on RECEIVE_POSSESSION", () => {
+
+    test( "player receives possession", () => {
+      const matchState = matchReducer( match, { type: "LOSE_POSSESSION" } );
+      const newState = matchReducer( matchState, { type: "RECEIVE_POSSESSION", playerId: 2 } )
+      expect( newState.getIn( ['home', 1, 'possession'] ) ).toBe( true );
+    })
+
+  })
+
 })
--- src/reducers/match.js
+++ src/reducers/match.js
@@ -5,6 +5,12 @@ export default function matchReducer( state = match, action ) {
   if (!action) { return state }
 
   switch ( action.type ) {
+    case "RECEIVE_POSSESSION":
+      const index = state.get( 'home' ).findIndex( (player) => { return player.get( 'id' ) === action.playerId } )
+      return state.setIn( ['home', index, 'possession' ], true )
+    case "LOSE_POSSESSION":
+      const playerIndex = state.get( 'home' ).toJS().findIndex( ( player ) => { return player.possession === true } )
+      return state.setIn( [ 'home', playerIndex, 'possession' ], false )
     default:
       return state
   }
